68630da4b2ba1f9e6ac9639d7fbe51099dc971cd
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-01-24 16:03:38 +0300

    Made general layout stylings for the site

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import styled from 'styled-components'
+import bg from './images/bg.png'
+import {MainLayout} from './styles/Layouts';
 
 function App() {
-
-
   return (
-    <div>Home</div>
+    <AppStyled bg={bg} className='App'>
+      <MainLayout>
+        
+      </MainLayout>
+    </AppStyled>
   )
 }
+
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url(${props => props.bg});
+  position: relative;
+`;
 
 export default App
