5bcf5c4f60080c8d55b8a3ebaaef73ec70303aad
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-01-31 10:39:07 +0300

    Started working on the navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,28 @@
-import styled from 'styled-components'
-import bg from './images/bg.png'
-import { MainLayout } from './styles/Layouts';
-import Orb from './Components/Orb/Orb';
-
+import React from "react";
+import styled from "styled-components";
+import bg from "./images/bg.png";
+import { MainLayout } from "./styles/Layouts";
+import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
+import { useState } from "react";
 
 function App() {
+  const [active, setActive] = useState(1);
+
   return (
-    <AppStyled bg={bg} className='App'>
+    <AppStyled bg={bg} className="App">
       <Orb />
       <MainLayout>
-
+        <Navigation active={active} setActive={setActive} />
       </MainLayout>
     </AppStyled>
-  )
+  );
 }
 
 const AppStyled = styled.div`
   height: 100vh;
-  background-image: url(${props => props.bg});
+  background-image: url(${(props) => props.bg});
   position: relative;
 `;
 
-export default App
+export default App;
