e99e1fb623992046f8b72e7b2c3c3023250c42e0
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-02-02 15:10:18 +0300

    Added the component files I will be using for my app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,27 @@ import bg from "./images/bg.png";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Incomes from "./Components/Incomes/Incomes";
+import Expenses from "./Components/Expenses/Expenses";
 
 function App() {
   const [active, setActive] = useState(1);
+
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />;
+      case 2:
+        return <Dashboard />;
+      case 3:
+        return <Incomes />;
+      case 4:
+        return <Expenses />;
+      default:
+        return <Dashboard />;
+    }
+  };
 
   const orbMemo = useMemo(() => {
     return <Orb />;
@@ -17,6 +35,7 @@ function App() {
       {orbMemo}
       <MainLayout>
         <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
       </MainLayout>
     </AppStyled>
   );
@@ -26,6 +45,17 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${(props) => props.bg});
   position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `;
 
 export default App;
---
+++ frontend/src/Components/Incomes/Incomes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../../styles/Layouts";
+
+const Incomes = () => {
+  return (
+    <IncomesStyled>
+      <InnerLayout></InnerLayout>
+    </IncomesStyled>
+  );
+};
+
+const IncomesStyled = styled.div``;
+
+export default Incomes;
