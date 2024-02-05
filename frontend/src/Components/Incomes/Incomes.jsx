d79db892efd843ba31152cd62709beff24525e65
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-02-05 13:27:27 +0300

    Made a globalcontext that should allow us to call the backend api we had made to handle income and expense data

--- frontend/src/Components/Incomes/Incomes.jsx
+++ frontend/src/Components/Incomes/Incomes.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import { useGlobalContext } from "../../context/globalContext";
 import { InnerLayout } from "../../styles/Layouts";
 
 const Incomes = () => {
+  const { addIncome } = useGlobalContext();
   return (
     <IncomesStyled>
-      <InnerLayout></InnerLayout>
+      <InnerLayout>
+        <h1>Incomes</h1>
+        <div className="income-content">
+          <div className="form-container"></div>
+          <div className="incomes"></div>
+        </div>
+      </InnerLayout>
     </IncomesStyled>
   );
 };
