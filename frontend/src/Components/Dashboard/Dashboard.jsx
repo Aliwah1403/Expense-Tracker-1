cb33cce9623735924a6f96bf12973eb9cba9cbd8
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-03-03 01:47:24 +0300

    Finished styling up the dashboard

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -60,10 +60,10 @@ const Dashboard = () => {
               <p>{Math.max(...incomes.map((item) => item.amount))}</p>
             </div>
 
-            <h2 className="income-title">
+            <h2 className="expense-title">
               Min <span>Expense</span>Max
             </h2>
-            <div className="income-item">
+            <div className="expense-item">
               <p>{Math.min(...expenses.map((item) => item.amount))}</p>
               <p>{Math.max(...expenses.map((item) => item.amount))}</p>
             </div>
@@ -74,6 +74,82 @@ const Dashboard = () => {
   );
 };
 
-const DashboardSytled = styled.div``;
+const DashboardSytled = styled.div`
+  .stats-con {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 2rem;
+    .chart-con {
+      grid-column: 1 / 4;
+      height: 400px;
+      .amount-con {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 2rem;
+        margin-top: 2rem;
+        .income,
+        .expense {
+          grid-column: span 2;
+        }
+        .income,
+        .expense,
+        .balance {
+          background: #fcf6f9;
+          border: 2px solid #ffffff;
+          box-shadow: var(--shadow-style);
+          border-radius: 20px;
+          padding: 1rem;
+          p {
+            font-size: 3.5rem;
+            font-weight: 700;
+          }
+        }
+        .balance {
+          grid-column: 2 /4;
+          display: flex;
+          justify-content: center;
+          flex-direction: column;
+          align-items: center;
+          p {
+            color: var(--color-green);
+            opacity: 0.6;
+            font-size: 4.5rem;
+          }
+        }
+      }
+    }
+    .history-con {
+      grid-column: 4 / -1;
+      h2 {
+        margin: 1rem 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+      .income-title,
+      .expense-title {
+        font-size: 1.2rem;
+        span {
+          font-size: 1.8rem;
+        }
+      }
+      .income-item,
+      .expense-item {
+        background: #fcf6f9;
+        border: 2px solid #ffffff;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        padding: 1rem;
+        border-radius: 20px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        p {
+          font-weight: 600;
+          font-size: 1.6rem;
+        }
+      }
+    }
+  }
+`;
 
 export default Dashboard;
