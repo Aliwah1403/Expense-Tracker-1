6c07f5b1a164f675258339d5037b5b8f2dd86eea
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-03-09 02:00:59 +0300

    used date-picker to filter out dates and only show transactions for selected dates on the transactions component

--- frontend/src/Components/History/TotalHistory.jsx
+++ frontend/src/Components/History/TotalHistory.jsx
@@ -1,38 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { useGlobalContext } from "../../context/globalContext";
 import styled from "styled-components";
 import { dateFormat } from "../../utils/dateFormat";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const TotalHistory = () => {
   const { totalTransactionHistory } = useGlobalContext();
 
-  const [...history] = totalTransactionHistory();
-  return (
-    <HistoryStyled>
-      {history.map((item) => {
-        const { _id, title, amount, type, date } = item;
-        return (
-          <div key={_id} className="history-item">
-            <p
-              style={{
-                color: type === "expense" ? "red" : "var(--color-green)",
-              }}
-            >
-              {title}
-            </p>
+  const [selectedDate, setSelectedDate] = useState(null);
 
-            <p
-              style={{
-                color: type === "expense" ? "red" : "var(--color-green)",
-              }}
-            >
-              {type === "expense" ? `-${amount}` : `+${amount}`}
-            </p>
-            <p>{dateFormat(date)}</p>
-          </div>
-        );
-      })}
-    </HistoryStyled>
+  const [...history] = totalTransactionHistory();
+
+  // using date-picker to filter out dates and only show transactions for selected date
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  const filteredHistory = selectedDate
+    ? history.filter((item) => {
+        return dateFormat(item.date) === dateFormat(selectedDate);
+      })
+    : history;
+  return (
+    <>
+      <div>
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          dateFormat="dd/MM/yyyy"
+        />
+      </div>
+      <HistoryStyled>
+        {filteredHistory.map((item) => {
+          const { _id, title, amount, type, date } = item;
+          return (
+            <div key={_id} className="history-item">
+              <p
+                style={{
+                  color: type === "expense" ? "red" : "var(--color-green)",
+                }}
+              >
+                {title}
+              </p>
+
+              <p
+                style={{
+                  color: type === "expense" ? "red" : "var(--color-green)",
+                }}
+              >
+                {type === "expense" ? `-${amount}` : `+${amount}`}
+              </p>
+              <p>{dateFormat(date)}</p>
+            </div>
+          );
+        })}
+      </HistoryStyled>
+    </>
   );
 };
 
