a6022e7f881aec56395b74ad7a619e0515a8e1d1
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-07-17 15:48:24 +0300

    refactor: Changed the design layout of the login page

--- frontend/src/auth/sign-in/SignIn.jsx
+++ frontend/src/auth/sign-in/SignIn.jsx
@@ -1,9 +1,26 @@
 import { SignIn } from "@clerk/clerk-react";
+import COVER_IMAGE from "../../../public/login-background.jpg";
 
 const SignInPage = () => {
   return (
-    <div className="flex justify-center my-20 items-center">
-      <SignIn />
+    // <div className="flex justify-center my-20 items-center">
+    //   <SignIn />
+    // </div>
+    <div className="w-full h-screen flex items-start">
+      <div className="relative w-1/2 h-full flex flex-col">
+        <div className="absolute top-[20%] left-[10%] flex flex-col">
+          <h1 className="text-4xl text-white font-bold my-4">
+            Track your finances with the most ease
+          </h1>
+          <p className="text-xl text-white font-normal">Start for free</p>
+        </div>
+        <img src={COVER_IMAGE} className="w-full h-full object-cover" />
+      </div>
+      <div className="w-1/2 h-full bg-[#f5f5f5] flex flex-col p-20">
+        <div className="w-full flex items-center justify-center">
+          <SignIn />
+        </div>
+      </div>
     </div>
   );
 };
