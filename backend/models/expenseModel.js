5aab11a1568773513cf07081f5c5a3f09ac48156
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-07-24 22:56:53 +0300

    feat: Added userId to the expense model

--- backend/models/expenseModel.js
+++ backend/models/expenseModel.js
@@ -1,40 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-
-const expenseSchema = new mongoose.Schema({
+const expenseSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
     title: {
-        type: String,
-        required: true,
-        trim: true,
-        maxLength: 50
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50,
     },
     amount: {
-        type: Number,
-        required: true,
-        maxLength: 20,
-        trim: true
+      type: Number,
+      required: true,
+      maxLength: 20,
+      trim: true,
     },
     type: {
-        type: String,
-        default: "expense"
+      type: String,
+      default: "expense",
     },
     date: {
-        type: Date,
-        required: true,
-        trim: true
+      type: Date,
+      required: true,
+      trim: true,
     },
     category: {
-        type: String,
-        required: true,
-        maxLength: 20,
-        trim: true
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
     },
     description: {
-        type: String,
-        required: true,
-        maxLength: 20,
-        trim: true
-    }
-}, { timestamps: true })
+      type: String,
+      required: true,
+      maxLength: 20,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('expense', expenseSchema);
+module.exports = mongoose.model("expense", expenseSchema);
