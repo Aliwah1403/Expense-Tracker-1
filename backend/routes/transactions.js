c8f64690b2a5550800bbb6beb251381211d1544d
Curtis Aliwah <curtisaliwah5@gmail.com>
2024-07-24 23:06:36 +0300

    refactor: Added clerkMiddleware to the routes file

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,14 +1,24 @@
-const { addExpense, deleteExpense, getExpense } = require('../controllers/expense');
-const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+const {
+  addExpense,
+  deleteExpense,
+  getExpense,
+} = require("../controllers/expense");
+const {
+  addIncome,
+  getIncomes,
+  deleteIncome,
+} = require("../controllers/income");
+const clerkMiddleware = require("../middleware/clerkMiddleware");
 
-const router = require('express').Router();
+const router = require("express").Router();
+router.use(clerkMiddleware);
 
-
-router.post('/add-income', addIncome)
-    .get('/get-incomes', getIncomes)
-    .delete('/delete-income/:id', deleteIncome)
-    .post('/add-expense', addExpense)
-    .get('/get-expenses', getExpense)
-    .delete('/delete-expense/:id', deleteExpense)
+router
+  .post("/add-income", addIncome)
+  .get("/get-incomes", getIncomes)
+  .delete("/delete-income/:id", deleteIncome)
+  .post("/add-expense", addExpense)
+  .get("/get-expenses", getExpense)
+  .delete("/delete-expense/:id", deleteExpense);
 
 module.exports = router;
